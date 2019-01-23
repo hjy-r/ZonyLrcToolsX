df4b2525afde9927374b747e84636b2e8965b741
GameBelial <243387971@qq.com>
2019-01-23 16:26:57 +0800

    fixed 解决 #20 提到的溢出问题，使用 string 代替 Id。

--- Zony.Lib.SingleLyricSearch/Startup.cs
+++ Zony.Lib.SingleLyricSearch/Startup.cs
@@ -34,8 +34,8 @@ namespace Zony.Lib.SongListDownload
             GlobalContext.Instance.UIContext.DisableTopButtons();
             ClearAllContainer();
 
-            int id = GetMusicDetailId();
-            if (id == 0)
+            var id = GetMusicDetailId();
+            if (string.IsNullOrEmpty(id))
             {
                 GlobalContext.Instance.UIContext.EnableTopButtons();
                 return;
@@ -85,7 +85,7 @@ namespace Zony.Lib.SongListDownload
         /// 对网易云 API 发起请求，获取歌单信息
         /// </summary>
         /// <param name="id">歌单 ID</param>
-        private NetEaseResultModel RequestNetEase(int id)
+        private NetEaseResultModel RequestNetEase(string id)
         {
             var jsonResult = _httpClient.Get("http://music.163.com/api/playlist/detail", new
             {
@@ -103,20 +103,20 @@ namespace Zony.Lib.SongListDownload
         /// <summary>
         /// 获取歌单 ID
         /// </summary>
-        private int GetMusicDetailId()
+        private string GetMusicDetailId()
         {
             var inputBox = new InputBox("请输入歌单 URL 或者歌单 ID", "网页版歌URL，例如:\r\n" +
                                          "http://music.163.com/#/playlist?id=716152005\r\n或者输入Id:716152005");
             inputBox.ShowDialog();
 
-            if (string.IsNullOrWhiteSpace(inputBox.ResultText)) return 0;
+            if (string.IsNullOrWhiteSpace(inputBox.ResultText)) return null;
 
-            if (int.TryParse(inputBox.ResultText, out int inputId)) return inputId;
+            if (new Regex("^[0-9]*$").IsMatch(inputBox.ResultText)) return inputBox.ResultText;
 
             var regex = new Regex(@"(?<=id=)\d+");
             var match = regex.Match(inputBox.ResultText);
-            if (!match.Success) return 0;
-            return int.Parse(match.Value);
+            if (!match.Success) return null;
+            return match.Value;
         }
 
         #region > UI 相关 <
