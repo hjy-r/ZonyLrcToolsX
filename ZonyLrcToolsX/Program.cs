da6d4baa4a3f901035df81aedaef16d054fbd7ef
GameBelial <243387971@qq.com>
2019-07-01 15:10:15 +0800

    增加全局异常处理功能;

--- ZonyLrcToolsX/Program.cs
+++ ZonyLrcToolsX/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZonyLrcToolsX.Infrastructure.Utils;
+
+// ReSharper disable LocalizableElement
 
 namespace ZonyLrcToolsX
 {
@@ -12,9 +16,22 @@ namespace ZonyLrcToolsX
         static void Main()
         {
             if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
+
+            // 全局异常处理。
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += async (sender, args) => await HandleException(args.ExceptionObject as Exception);
+            Application.ThreadException += async (sender, args) => await HandleException(args.Exception);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static async Task HandleException(Exception exception)
+        {
+            MessageBox.Show("产生了未经处理的异常，请将程序目录下的 exception.log 文件发送给作者，程序即将退出。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            await LogUtils.Instance.WriteExceptionAsync(exception);
+            ProgramUtils.ExitProgram();
         }
     }
 }
