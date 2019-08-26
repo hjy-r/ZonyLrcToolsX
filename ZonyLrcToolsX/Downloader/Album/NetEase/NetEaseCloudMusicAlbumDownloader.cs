5be4107f969c85c78f2580bf34fe714830f109f6
GameBelial <243387971@qq.com>
2019-08-26 22:01:58 +0800

    Add new ablum downloader.

---
+++ ZonyLrcToolsX/Downloader/Album/NetEase/NetEaseCloudMusicAlbumDownloader.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace ZonyLrcToolsX.Downloader.Album.NetEase
+{
+    /// <summary>
+    /// 基于网易云音乐实现的专辑下载器，用于下载歌曲的专辑封面。
+    /// </summary>
+    public class NetEaseCloudMusicAlbumDownloader : IAlbumDownloader
+    {
+        public Task<byte[]> DownloadAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
