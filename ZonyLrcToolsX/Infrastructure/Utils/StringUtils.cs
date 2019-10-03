471a94d3c041e6a22e850ce27860a4258efd4db6
GameBelial <243387971@qq.com>
2019-10-03 14:51:41 +0800

    增加针对 String 参数的 TrimEnd 扩展方法。

--- ZonyLrcToolsX/Infrastructure/Utils/StringUtils.cs
+++ ZonyLrcToolsX/Infrastructure/Utils/StringUtils.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ZonyLrcToolsX.Infrastructure.Utils
 {
     public static class StringUtils
@@ -5,6 +7,16 @@ namespace ZonyLrcToolsX.Infrastructure.Utils
         public static bool IsNullOrEmptyOrWhite(this string srcStr)
         {
             return string.IsNullOrEmpty(srcStr) && string.IsNullOrWhiteSpace(srcStr);
+        }
+
+        public static string TrimEnd(this string srcStr, string trimEndStr)
+        {
+            if (srcStr.EndsWith(trimEndStr,StringComparison.Ordinal))
+            {
+                return srcStr.Substring(0,srcStr.Length - trimEndStr.Length);
+            }
+
+            return srcStr;
         }
     }
 }
