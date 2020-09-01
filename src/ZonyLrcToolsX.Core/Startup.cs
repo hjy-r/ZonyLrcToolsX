34fa01b76ae19c36837b109893b38cd1f0a4a11c
GameBelial <243387971@qq.com>
2020-09-01 21:47:03 +0800

    Integrated react.

--- src/ZonyLrcToolsX.Core/Startup.cs
+++ src/ZonyLrcToolsX.Core/Startup.cs
@@ -1,8 +1,9 @@
 using System.Threading.Tasks;
 using ElectronNET.API;
+using ElectronNET.API.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,19 +13,52 @@ namespace ZonyLrcToolsX.Core
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ReactApp/build"; });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            
             app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+            
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ReactApp";
 
-            app.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
-            Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+
+            if (HybridSupport.IsElectronActive)
+            {
+                Task.Run(ConfigureElectronBootstrap);
+            }
+        }
+
+        private async Task ConfigureElectronBootstrap()
+        {
+            var browserWindow = await Electron.WindowManager.CreateWindowAsync(new BrowserWindowOptions
+            {
+                Height = 600,
+                Width = 800,
+                Show = false
+            });
+
+            await browserWindow.WebContents.Session.ClearCacheAsync();
+
+            browserWindow.OnReadyToShow += () => browserWindow.Show();
+            browserWindow.SetTitle("ZonyLrcToolsX");
         }
     }
 }
