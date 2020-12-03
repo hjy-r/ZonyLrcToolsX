075cbfac8f20d07186c20adc75a16a5356733318
GameBelial <243387971@qq.com>
2020-12-03 23:46:10 +0800

    Add lyric configuration class.

---
+++ src/ZonyLrcToolsX/Infrastructure/Lyric/LyricItemCollectionOption.cs
@@ -0,0 +1,15 @@
+namespace ZonyLrcToolsX.Infrastructure.Lyric
+{
+    public class LyricItemCollectionOption
+    {
+        /// <summary>
+        /// 双语歌词是否合并为一行。
+        /// </summary>
+        public bool IsOneLine { get; set; }
+
+        /// <summary>
+        /// 换行符格式，取值来自 <see cref="LineBreakType"/> 常量类。
+        /// </summary>
+        public string LineBreak { get; set; }
+    }
+}
